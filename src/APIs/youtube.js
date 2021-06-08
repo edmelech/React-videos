624caa78583ffb6b07b0fa32a264fb6bf544b019
Ed Melech <mr.melech@gmail.com>
2021-06-08 15:33:27 +0100

    install axios

--- src/APIs/youtube.js
+++ src/APIs/youtube.js
@@ -4,4 +4,10 @@ const KEY = 'AIzaSyDdzP6bDduhuJJK0iuQ1M3Lgf5Mh1ofsAo';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video'
+    maxResults: 5,
+    key: KEY
+  }
 });
