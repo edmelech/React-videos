d73903a22af8ffb8e6b72cba55c59448863714fd
Ed Melech <mr.melech@gmail.com>
2021-06-10 15:49:41 +0100

    add if statemenet to VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) =>  {
-  return <div>{video.snippet.title}</div>
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{video.snippet.title}</div>;
 };
 
 export default VideoDetail;
