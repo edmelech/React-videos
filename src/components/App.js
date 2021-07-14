8e158c6018431b47218eef5e2ab818580da8c4b0
Ed Melech <mr.melech@gmail.com>
2021-07-14 14:58:32 +0100

    declare ‘videos’ and ‘selectedVideo’ pieces of state.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../APIs/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+};
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
