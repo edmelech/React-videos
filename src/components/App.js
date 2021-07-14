4c817aa96ef6f4549600dbcde87c13e90c18f465
Ed Melech <mr.melech@gmail.com>
2021-07-14 15:28:35 +0100

    refactor app using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -29,16 +29,16 @@ const App = () => {
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={this.onTermSubmit} />
+      <SearchBar onFormSubmit={onTermSubmit} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
-            <VideoDetail video={this.state.selectedVideo} />
+            <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
             <VideoList
-              onVideoSelect={this.onVideoSelect}
-              videos={this.state.videos}
+              onVideoSelect={onVideoSelect}
+              videos={videos}
             />
           </div>
         </div>
@@ -46,13 +46,5 @@ const App = () => {
     </div>
   );
 };
-
-class App extends React.Component {
-
-
-  render() {
-
-  }
-}
 
 export default App;
