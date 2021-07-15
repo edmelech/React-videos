2671625da3f23facde2ec4cf4727adaaaa1e29aa
Ed Melech <mr.melech@gmail.com>
2021-07-15 14:06:55 +0100

    use custom hook

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -5,14 +5,14 @@ const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    search(defaultSearchTerm)
-  }, []);
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
 
    const search = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
-      }
+      },
     });
 
     setVideos(response.data.items);
